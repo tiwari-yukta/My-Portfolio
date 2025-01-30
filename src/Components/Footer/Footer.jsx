a4541ea1f1e87522eeb5b178654e893d944a4c9e
Yukta Tiwari <tiwariyukta55@gmail.com>
2025-01-30 12:18:36 +0530

    Footer changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,40 @@
+// import React from "react";
+// import "./Footer.css";
+// const Footer = () => {
+//   return (
+//     <div className="footer">
+//       <div className="footer-top"></div>
+//       <hr />
+//       <div className="footer-bottom">
+//         <div className="footer-bottom-right flex justify-centre">
+//           <p>Term of Services</p>
+//           <p>Privacy Policies</p>
+//           <p>Connect with me</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
 import React from "react";
 import "./Footer.css";
+
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <div className="footer-top"></div>
       <hr />
       <div className="footer-bottom">
-        <div className="footer-bottom-right">
-          <p>Term of Services</p>
-          <p>Privacy Policies</p>
-          <p>Connect with me</p>
+        <div className="footer-content">
+          <p>© {new Date().getFullYear()} Yukta Tiwari. All Rights Reserved.</p>
+          <div className="footer-links">
+            <a href="#">Terms of Service</a>
+            <a href="#">Privacy Policy</a>
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
