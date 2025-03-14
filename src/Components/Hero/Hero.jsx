2e873bbfddda099245073a0ac2acef8e0a751e01
Yukta Tiwari <yuktatiwari738@gmail.com>
2025-03-14 11:14:58 +0530

    Updated Resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
           </AnchorLink>
         </div>
         <div className="hero-resume">
-          <a href="https://drive.google.com/file/d/1Rv1fedqFBFZPy28u_e0yGAeMyyWSGIZd/view?usp=drivesdk">
+          <a href="https://drive.google.com/file/d/1GY0itRbOnmSQVsINI4ZPDXTOGpSOG4Au/view?usp=sharing">
             My Resume
           </a>
         </div>
