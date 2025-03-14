c8a2008a883ad1ee8240a1606836212574194526
Yukta Tiwari <yuktatiwari738@gmail.com>
2025-03-14 11:31:44 +0530

    Added projects

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -7,15 +7,30 @@ const Projects_Data = [
       "https://plus.unsplash.com/premium_photo-1673953509975-576678fa6710?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZG9jdG9yc3xlbnwwfHwwfHx8MA%3D%3D",
     link: "https://tiwari-yukta.github.io/Health-and-Care/",
   },
-
   {
-    name: "Food Website",
-    technologies: "HTML, CSS",
-    description: "A responsive food website.",
-    image:
-      "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHdlYnNpdGV8ZW58MHwxfDB8fHwy",
-    link: "https://tiwari-yukta.github.io/ILoveCake.github.io/",
-  },
+    "name": "Express Authenticator",
+    "technologies": "Express.js, MongoDB, JWT, Node.js",
+    "description": "User authentication system.",
+    "image": "https://images.unsplash.com/photo-1618044619888-009e412ff12a?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "link": "https://github.com/tiwari-yukta/express-auth-app"
+  }
+,  
+{
+  "name": "Quiz Platform",
+  "technologies": "React, Tailwind CSS",
+  "description": "A quiz platform with categories like DSA, OS, DBMS, and CN.",
+  "image": "https://images.unsplash.com/photo-1524678714210-9917a6c619c2?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHF1aXp8ZW58MHx8MHx8fDI%3D",
+  "link": "https://github.com/tiwari-yukta/quiz-platform"
+}
+,
+  // {
+  //   name: "Food Website",
+  //   technologies: "HTML, CSS",
+  //   description: "A responsive food website.",
+  //   image:
+  //     "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMHdlYnNpdGV8ZW58MHwxfDB8fHwy",
+  //   link: "https://tiwari-yukta.github.io/ILoveCake.github.io/",
+  // },
   {
     name: "Password Generator",
     technologies: "React, JS, CSS",
@@ -24,13 +39,13 @@ const Projects_Data = [
       "https://images.unsplash.com/photo-1724219616919-aab943e7b00d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBhc3N3b3JkfGVufDB8fDB8fHww",
     link: "https://tiwari-yukta.github.io/password-generator/",
   },
-  {
-    name: "Keeper App",
-    technologies: "React, HTML, CSS",
-    description: "Keeping the things intact .",
-    image:
-      "https://plus.unsplash.com/premium_photo-1681487870238-4a2dfddc6bcb?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG8lMjBkbyUyMGxpc3R8ZW58MHx8MHx8fDA%3D",
-    link: "https://tiwari-yukta.github.io/keeper-app/",
-  },
+  // {
+  //   name: "Keeper App",
+  //   technologies: "React, HTML, CSS",
+  //   description: "Keeping the things intact .",
+  //   image:
+  //     "https://plus.unsplash.com/premium_photo-1681487870238-4a2dfddc6bcb?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG8lMjBkbyUyMGxpc3R8ZW58MHx8MHx8fDA%3D",
+  //   link: "https://tiwari-yukta.github.io/keeper-app/",
+  // },
 ];
 export default Projects_Data;
